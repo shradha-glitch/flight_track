c86200999d85a53261a952923d8af0e4da5bdeb1
AmandaArbinge <62724335+AmandaArbinge@users.noreply.github.com>
2025-02-20 11:55:17 +0100

    Frontend structure setup, bootstrapped using react, added d3.js and material UI

---
+++ frontend/src/components/Button.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function ButtonUsage() {
+  return <>
+  <h1>Journey</h1>
+  <Button variant="contained">Hello world</Button>
+  </>
+}
