e064bac62a6c2b8329453ac2d586be2b5c20a723
AmandaArbinge <62724335+AmandaArbinge@users.noreply.github.com>
2025-03-02 14:25:56 +0100

    added some styling to inputs and added search button

--- frontend/src/components/card/PcpCard.js
+++ frontend/src/components/card/PcpCard.js
@@ -1,34 +1,54 @@
 import CustomCard from "./Card";
 import ParallelCoordinates from "../PCP";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import Departure from "../user_input/Location";
 import PassportInput from "../user_input/Passport";
 import DatePicker from "../user_input/Date";
 import Header from "../Header";
+import SearchIcon from "@mui/icons-material/Search";
 
 const PcpCard = ({ onSelect }) => {
   return (
     <CustomCard>
-    <Header></Header>
+      <Header></Header>
       <Box
         sx={{
-            mb: 4,
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-            gap: 2,
-            px: 4,
+          mb: 4,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          gap: 2,
+          px: 4,
+          flexWrap: "wrap"
         }}
       >
         <Departure onSelect={onSelect} />
-        <DatePicker label={"Departure"} />
-        <DatePicker label={"Return"} />
+        <DatePicker label={"Departure"} text={"Dates of Travel"} />
+        <DatePicker label={"Return"} text={" fef"}/>
         <PassportInput />
+        <Button
+          variant="contained"
+          startIcon={<SearchIcon />}
+          sx={{
+            py:2,
+            px:4,
+            maxHeight:55,
+            maxWidth:200,
+            fontSize: 16,
+            textTransform: "none",
+            backgroundColor: "#F1C120",
+            color: "#000",
+            "&:hover": {
+              backgroundColor: "#D8AD1C",
+            },
+          }}
+        >
+          Search
+        </Button>
       </Box>
-      <Box
-      sx={{px: 4}}>
-      <ParallelCoordinates/> 
+      <Box sx={{ px: 4 }}>
+        <ParallelCoordinates />
       </Box>
     </CustomCard>
   );
