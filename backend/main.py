8b99d91510c0c348bb80913f399b0f817f4a1523
shradha-glitch <shradzretharekar@gmail.com>
2025-03-09 14:20:48 +0100

    trying to commit

--- backend/main.py
+++ backend/main.py
@@ -38,6 +38,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",  # Local development
         "https://flight-track-1.onrender.com"  # Add your Render frontend URL here
+
     ],
     allow_credentials=True,
     allow_methods=["*"],
